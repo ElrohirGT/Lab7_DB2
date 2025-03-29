953e2e9c9f1c3b4b215d9dbad17d542942c7f7f9
ElrohirGT <elrohirgt@gmail.com>
2025-03-29 14:00:37 -0600

    feat: Added MongoDB migration logic

--- etl_code/main.js
+++ etl_code/main.js
@@ -1,8 +1,8 @@
 import pg from "pg";
+const { Client } = pg;
 import { MongoClient } from "mongodb";
 
 // PG Connection...
-const { Client } = pg;
 const PG_RESULT_CLIENT = new Client({
 	user: "admin",
 	password: "1234",
@@ -24,17 +24,79 @@ const PG_SOURCE_CLIENT = new Client({
 const uri = "mongodb://localhost:27018/turistic_costs";
 const MONGO_SOURCE_CLIENT = new MongoClient(uri);
 
+/**
+ * @typedef {Object} ETLRow
+ * @property {string} id
+ * @property {string} continente
+ * @property {string} pais
+ * @property {string} capital
+ * @property {string} region
+ * @property {string} poblacion
+ * @property {string} tasaEnvejecimiento
+ * @property {string} costoBajoHospedaje
+ * @property {string} costoPromedioComida
+ * @property {string} costoBajoTransporte
+ * @property {string} costoPromedioEntretenimiento
+ */
+
+/**
+ * @param {ETLRow} data
+ * @param {Client} conn
+ */
+async function insertETLRow(conn, data) {
+	const query =
+		"insert into metrics ( continente, pais, capital, region, poblacion, tasa_de_envejecimiento, costo_bajo_hospedaje, costo_promedio_comida, costo_bajo_transporte, costo_promedio_entretenimiento) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)";
+	const params = [
+		data.continente,
+		data.pais,
+		data.capital,
+		data.region,
+		data.poblacion,
+		data.tasaEnvejecimiento,
+		data.costoBajoHospedaje,
+		data.costoPromedioComida,
+		data.costoBajoTransporte,
+		data.costoPromedioEntretenimiento,
+	];
+
+	return conn.query(query, params);
+}
+
 try {
+	await PG_RESULT_CLIENT.connect();
+	PG_RESULT_CLIENT.query("DELETE FROM metrics;", []);
+	await PG_SOURCE_CLIENT.connect();
+
 	const database = MONGO_SOURCE_CLIENT.db("turistic_costs");
 	const movies = database.collection("general_costs");
 
-	// Query for a movie that has the title 'Back to the Future'
 	const query = {};
-	const movie = await movies.findOne(query);
+	const cursor = movies.find(query);
+	while (await cursor.hasNext()) {
+		const next = await cursor.next();
+		if (next === null) {
+			throw new Error("NEXT CANT BE NULL!");
+		}
+		console.log(next);
 
-	console.log(movie);
-	await PG_RESULT_CLIENT.connect();
-	await PG_SOURCE_CLIENT.connect();
+		const costos = next.costos_diarios_estimados_en_dólares;
+		/** @type {ETLRow} */
+		const data = {
+			continente: next.continente,
+			pais: next.país,
+			capital: next.capital,
+			region: next.región,
+			poblacion: next.población,
+			tasaEnvejecimiento: next.tasaEnvejecimiento,
+			costoBajoHospedaje: costos?.hospedaje?.precio_bajo_usd,
+			costoPromedioComida: costos?.comida?.precio_promedio_usd,
+			costoBajoTransporte: costos?.transporte?.precio_bajo_usd,
+			costoPromedioEntretenimiento:
+				costos?.entretenimiento?.precio_promedio_usd,
+		};
+
+		await insertETLRow(PG_RESULT_CLIENT, data);
+	}
 } catch (e) {
 	console.error(e);
 } finally {
